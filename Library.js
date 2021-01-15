4d4fcfd27019864e3cf74dff08dca42e345a9ddc
Zim Agu <zimmagu@gmail.com>
2021-01-15 11:43:35 -0800

    The project is finished

--- Library.js
+++ Library.js
@@ -14,9 +14,10 @@
 
 
 
-
-
-// Parent class for the Book, Movie, & CD Subclasses
+//////////////////////////////////////////////////////////
+//                   Media Class                        //                   
+//  Parent class for the Book, Movie, & CD Subclasses   //
+//////////////////////////////////////////////////////////
 class Media {
 
 
@@ -77,7 +78,7 @@ class Media {
 
 
     // Getter and Setters for the isCheckedOut property
-    get set_isCheckedOut () {
+    get get_isCheckedOut () {
         return this._isCheckedOut;
     }
 
@@ -199,8 +200,11 @@ class Media {
             in the rating array to geth the average
 
             Stores the sum of the ratings into the variable averge_Rating
+
+            The toFixed method rounds the avergae rating to a certain amount
+            of decimal places
         */
-        let averge_Rating = sum_of_Ratings / amount_of_Ratings;
+        let averge_Rating = ( sum_of_Ratings / amount_of_Ratings ).toFixed(2);
 
 
 
@@ -281,7 +285,7 @@ class Book extends Media{
 
 
 
-    constructor(title, author, pages) {
+    constructor(author, title, pages) {
 
         super(title);
         
@@ -375,7 +379,7 @@ class Movie extends Media {
 
 
 
-    constructor(title, director, runtime) {
+    constructor(director, title, runtime) {
         
         super(title);
 
@@ -441,6 +445,11 @@ class Movie extends Media {
 
 
 
+
+
+
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                         End of Class Declarations                                           //                                        
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -459,3 +468,124 @@ class Movie extends Media {
 
 
 
+
+
+/*  Book Object
+
+    Author: Bill Bryson
+    Title: A Short History of Nearly Everything
+    Pages: 544
+
+*/
+const history_Of_Everything = new Book(`Bill Bryson`, `A Short History of Nearly Everything`, 544);
+
+
+
+
+
+
+
+
+// Changed the check out statur of the history_Of_Everything to true
+history_Of_Everything.toggleCheckOutStatus();
+
+
+
+
+
+
+
+
+
+/* 
+    If the book is checked out 
+    the program logs to the screen that is is now checked out
+
+    If the book is not checked out
+    the program logs to the screen that it is not checked out
+*/
+if (history_Of_Everything.get_isCheckedOut == true) {
+    
+    console.log(`The book "${history_Of_Everything.get_title}" is currently checked out`);
+
+} else {
+
+    console.log(`The book "${history_Of_Everything.get_title}" is currently available`);
+
+}
+
+
+
+
+
+// Adding 3 ratings to the history of everything book
+history_Of_Everything.addRating(4);
+history_Of_Everything.addRating(7);
+history_Of_Everything.addRating(5);
+
+
+// Stores the average of the rating in the HOE_Averge_Rating variable
+let HOE_Averge_Rating = history_Of_Everything.getAvergaeRating();
+
+
+// Prints the average rating of the book to the screen
+console.log(`The average rating of this book is a ${HOE_Averge_Rating} out of 10`);
+
+
+
+
+
+
+
+
+console.log(``);
+
+
+
+
+
+
+
+
+
+
+
+
+/*  Movie Object
+
+    Director: Jan de Bont
+    Title: Speed
+    Runtime: 116
+    
+*/
+
+const speed = new Movie(`Jane de Bont`, `Speed`, 116);
+
+
+
+
+speed.toggleCheckOutStatus();
+
+
+
+
+if (speed.get_isCheckedOut == true) {
+    
+    console.log(`The Movie "${speed.get_title}" is currently checked out`);
+
+} else {
+
+    console.log(`The Movie "${speed.get_title}" is currently available`);
+
+}
+
+
+speed.addRating(3);
+speed.addRating(5);
+speed.addRating(1);
+
+
+let speed_Average_Rating = speed.getAvergaeRating();
+
+
+console.log(`The average rating of this Movies is a ${speed_Average_Rating} out of 10`);
